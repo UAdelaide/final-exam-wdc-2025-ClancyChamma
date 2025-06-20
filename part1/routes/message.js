4c0d84ef341111efab42087e8a177f626699ffdc
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-20 08:32:36 +0000

    [automatic commit] 2025-06-20 08:32:35 UTC 115.166.26.52

--- part1/routes/message.js
+++ part1/routes/message.js
@@ -24,20 +24,20 @@ router.get('/walkrequests/open', async (req, res) => {
 });
 
 
-router.get('/walkers/summary', async (req, res) => {
-        const [rows] = await db.query(`
-            SELECT
-                u.username AS walker_username,
-                COUNT(wr.request_id) AS total_ratings,
-                AVG(wr.rating) AS average_rating,
-                COUNT(DISTINCT wa.request_id) AS total_walks
-            FROM Users u
-            LEFT JOIN WalkApplications wa ON u.user_id = wa.walker_id
-            LEFT JOIN WalkRatings wr ON u.user_id = wr.walker_id
-            WHERE u.role = 'walker'
-            GROUP BY u.user_id
-        `);
-        res.json(rows);
-});
+// router.get('/walkers/summary', async (req, res) => {
+//         const [rows] = await db.query(`
+//             SELECT
+//                 u.username AS walker_username,
+//                 COUNT(wr.request_id) AS total_ratings,
+//                 AVG(wr.rating) AS average_rating,
+//                 COUNT(DISTINCT wa.request_id) AS total_walks
+//             FROM Users u
+//             LEFT JOIN WalkApplications wa ON u.user_id = wa.walker_id
+//             LEFT JOIN WalkRatings wr ON u.user_id = wr.walker_id
+//             WHERE u.role = 'walker'
+//             GROUP BY u.user_id
+//         `);
+//         res.json(rows);
+// });
 
 module.exports = router;
