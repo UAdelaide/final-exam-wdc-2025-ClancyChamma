4da3194af3bf208943bba1be3c328a64ec5e229b
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-20 08:03:44 +0000

    [automatic commit] 2025-06-20 08:03:43 UTC 115.166.26.52

--- part1/routes/message.js
+++ part1/routes/message.js
@@ -19,9 +19,13 @@ router.get('/api/walkrequests/open', async (req, res) => {
 
 // router.get('/api/walkrequests/open', async (req, res) => {
 //             const [rows] = await db.query(`
-//             SELECT d.name AS dog_name, d.size, u.username AS owner_username
-//             FROM Dogs d
-//             JOIN Users u ON d.owner_id = u.user_id
+            SELECT
+            u.username AS walker_username,
+            COUNT(wr.request_id) AS total_ratings,
+            AVG(wr.rating) AS average_rating,
+            COUNT(DISTINCT wa.request_id) AS total_walks
+            FROM Users u
+            LEFT JOIN WalkRequests wr ON u.user_id = wr.walker_id
 //             `);
 // });
 
