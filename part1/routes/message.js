1b39512f7423324c4cbd5b9c4806b4a7a3af4fb5
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-20 07:37:21 +0000

    [automatic commit] 2025-06-20 07:37:20 UTC 115.166.26.52

--- part1/routes/message.js
+++ part1/routes/message.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var db = require('../db');
 
 router.get('/api/dogs', async (req, res) => {
-    ``
+    try {
+        const [dogs] = await db.execute('SELECT * FROM dogs');
+
 })
