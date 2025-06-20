7b14af0842a07b301a6a372cc25b7a174790012e
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-20 23:38:19 +0000

    [automatic commit] 2025-06-20 23:38:19 UTC 115.166.26.52

--- part2/app.js
+++ part2/app.js
@@ -11,6 +11,8 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
     secret: '05ac256a9dd497ef1e4da0af47a047291a30fc2053c3965e092da9bb5470ec8a',
     resave: false,
+    saveUninitialized: true,
+    cookie: { se}
 }))
 
 // Routes
