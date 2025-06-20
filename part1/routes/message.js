ca22e95700f973b1c0b1c15eec92e691c1e28f20
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-20 08:09:20 +0000

    [automatic commit] 2025-06-20 08:09:19 UTC 115.166.26.52

--- part1/routes/message.js
+++ part1/routes/message.js
@@ -17,6 +17,8 @@ router.get('/api/walkrequests/open', async (req, res) => {
             wr.duration_minutes, wr.location, u.username AS owner_username
             FROM WalkRequests wr
             JOIN Dogs d ON wr.dog_id = d.dog_id
+            JOIN Users u ON u.owner_id = u.user_id
+            WHERE wr.status = 'open'
             `);
 });
 
