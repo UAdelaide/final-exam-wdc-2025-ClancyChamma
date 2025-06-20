cd0de2ed7f996ee30e69df5f6261bb6bde8306f5
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-20 07:48:10 +0000

    [automatic commit] 2025-06-20 07:48:09 UTC 115.166.26.52

--- part1/db.js
+++ part1/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    socketPath: '/var/run/mysqld/mysqld.sock', // Adjust this path if necessary
+    socketPath: '/var/run/mysqld/mysqld.sock', 
     host: 'localhost',
     user: 'root',
     password: 'mypassword',
