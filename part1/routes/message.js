2f71ca9ac7f7271b47f362e41f96ac7795fd5128
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-20 07:58:23 +0000

    [automatic commit] 2025-06-20 07:58:23 UTC 115.166.26.52

--- part1/routes/message.js
+++ part1/routes/message.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require('../db');
 
 router.get('/dogs', async (req, res) => {
-        const [rows] = await db.query(`
+        const [dogs] = await db.query(`
             SELECT d.name AS dog_name, d.size, u.username AS owner_username
             FROM Dogs d
             JOIN Users u ON d.owner_id = u.user_id
