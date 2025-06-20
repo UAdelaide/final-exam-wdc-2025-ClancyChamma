31d0c4be29088ab908e0375844382daffda4b3ef
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-20 08:05:57 +0000

    [automatic commit] 2025-06-20 08:05:56 UTC 115.166.26.52

--- part1/routes/message.js
+++ part1/routes/message.js
@@ -17,17 +17,19 @@ router.get('/api/walkrequests/open', async (req, res) => {
             `);
 });
 
-router.get('/api/walkers/summary', async (req, res) => {
-        const [rows] = await db.query(`
-            SELECT
-                u.username AS walker_username,
-                COUNT(wr.request_id) AS total_ratings,
-                AVG(wr.rating) AS average_rating,
-                COUNT(DISTINCT wa.request_id) AS total_walks
-            FROM Users u
-            LEFT JOIN WalkRequests wr ON u.user_id = wr.walker_id
-        `);
-        res.json(rows);
-});
+ro
+
+// router.get('/api/walkers/summary', async (req, res) => {
+//         const [rows] = await db.query(`
+//             SELECT
+//                 u.username AS walker_username,
+//                 COUNT(wr.request_id) AS total_ratings,
+//                 AVG(wr.rating) AS average_rating,
+//                 COUNT(DISTINCT wa.request_id) AS total_walks
+//             FROM Users u
+//             LEFT JOIN WalkRequests wr ON u.user_id = wr.walker_id
+//         `);
+//         res.json(rows);
+// });
 
 module.exports = router;
