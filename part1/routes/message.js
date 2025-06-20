21dedbf6bee9465ecef7b7cee60cd14772369b99
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-20 07:39:12 +0000

    [automatic commit] 2025-06-20 07:39:11 UTC 115.166.26.52

--- part1/routes/message.js
+++ part1/routes/message.js
@@ -6,5 +6,6 @@ router.get('/api/dogs', async (req, res) => {
     try {
         const [rows] = await db.query(`
             SELECT d.name AS dog_name, d.size, u.username AS owner_username
-            `)
+            FROM Dogs d
+            JOIN Users u ON d.owner_id = u.userid`)
 })
