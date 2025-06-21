c2f669d75a695832ce5c81eb6ee9a3cb7e0510a1
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-21 02:08:18 +0000

    [automatic commit] 2025-06-21 02:08:17 UTC 115.166.26.52

--- part2/app.js
+++ part2/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
-    secret: pro,
+    secret: proccess.env.SESSION_SECRET || '',
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }
