d15563de5f41b281a108aae08edacc47fd2fafef
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-21 03:17:18 +0000

    [automatic commit] 2025-06-21 03:17:18 UTC 115.166.26.52

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -193,7 +193,8 @@ function login(){
                 window.location.href = 'owner-dashboard.html';
             } else if (response.role === 'walker') {
                 window.location.href = 'walker-dashboard.html';
-            }
+            } else {
+                alert("Unknown role: " + response.role);
             }
         } else if (this.readyState == 4 && this.status >= 400) {
             alert("Login failed");
