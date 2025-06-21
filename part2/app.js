8b226edfd3b8bbcbce04e6dcf5b06a4eab88707c
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-21 01:16:43 +0000

    [automatic commit] 2025-06-21 01:16:43 UTC 115.166.26.52

--- part2/app.js
+++ part2/app.js
@@ -8,6 +8,9 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(session({
+    secret: '05ac256a9dd497ef1e4da0af47a047291a30fc2053c3965e092da9bb5470ec8a',
+}))
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
