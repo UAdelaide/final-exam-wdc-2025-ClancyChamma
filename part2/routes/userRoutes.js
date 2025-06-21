f3fd74e16d70ad89a41802a2d7caadd2b198fd5e
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-21 00:44:08 +0000

    [automatic commit] 2025-06-21 00:44:08 UTC 115.166.26.52

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -62,7 +62,8 @@ router.post('/logout', (req, res) => {
     return res.status(500).json({ error: 'Logout failed' });
   }
   res.clearCookie('connect.sid');
-  res.json
+  res.json({ message: 'Logged out successfully' });
+})
 }
 
 module.exports = router;
